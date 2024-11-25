8fa79ab6c970ac3e2e176e12ed8d8dabc4a9814e
Roschlynn Michael Dsouza <roschlynn@outlook.com>
2024-11-25 01:05:44 -0600

    Removed stupid ethereum code

--- python_scripts/eth_account_maker/eth_account_generator.py
+++
@@ -1,8 +0,0 @@
-from eth_account import Account
-import secrets
-
-def generate_eth_address():
-    priv = secrets.token_bytes(32)
-    private_key = '0x' + priv.hex()
-    account = Account.from_key(private_key)
-    return account.address, private_key
